45f582b4a8ee5f58045878feee6c43e5d239a0c3
Maxim Zhiburt <zhiburt@gmail.com>
2021-06-23 00:44:37 +0300

    Init
    Signed-off-by: Maxim Zhiburt <zhiburt@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,169 @@
+use ansi_parser::AnsiSequence;
+use ansi_parser::{AnsiParser, Output};
+use std::ops::{Bound, RangeBounds};
+
+pub fn cut<S, R>(string: S, bounds: R) -> String
+where
+    S: AsRef<str>,
+    R: RangeBounds<usize>,
+{
+    let string = string.as_ref();
+    let string_width = str_len(string);
+    let (start, end) = bounds_to_usize(bounds.start_bound(), bounds.end_bound(), string_width);
+
+    assert!(start <= end);
+    assert!(end <= string_width);
+
+    cut_str(string, start, end)
+}
+
+fn cut_str(string: &str, start: usize, end: usize) -> String {
+    let parsed: Vec<Output> = string.ansi_parse().collect();
+
+    let mut start = start;
+    let mut end = end;
+    let mut need = end - start;
+    let mut buffer = String::with_capacity(start + end);
+    let mut index = 0;
+    let mut escapes = Vec::new();
+    for block in parsed.into_iter() {
+        match block {
+            Output::TextBlock(text) => {
+                if need == 0 {
+                    break;
+                }
+
+                let block_len = str_len(text);
+                let is_nesessary_block = start < block_len;
+                if is_nesessary_block {
+                    escapes
+                        .iter()
+                        .for_each(|esc: &AnsiSequence| buffer.push_str(esc.to_string().as_str()));
+                }
+
+                if is_nesessary_block {
+                    let lhs = start;
+                    let rhs = std::cmp::min(block_len, start + need);
+
+                    let block = text.chars().skip(lhs).take(rhs - lhs);
+
+                    start = 0;
+                    buffer.extend(block);
+                    need -= rhs - lhs;
+
+                    if escapes.is_empty() && need == 0 {
+                        break;
+                    }
+                }
+
+                if !is_nesessary_block {
+                    start -= block_len;
+                }
+
+                index += block_len;
+
+                escapes.clear();
+            }
+            Output::Escape(seq) => {
+                if need == 0 {
+                    if let AnsiSequence::SetGraphicsMode(_) = seq {
+                        buffer.push_str(seq.to_string().as_str())
+                    }
+                } else {
+                    escapes.push(seq)
+                }
+            }
+        }
+    }
+
+    buffer
+}
+
+fn str_len(string: &str) -> usize {
+    let bytes = strip_ansi_escapes::strip(string).expect("Wierd things happen");
+    let string = String::from_utf8_lossy(&bytes);
+    let len = string.chars().count();
+    len
+}
+
+fn bounds_to_usize(
+    left: Bound<&usize>,
+    right: Bound<&usize>,
+    count_elements: usize,
+) -> (usize, usize) {
+    match (left, right) {
+        (Bound::Included(x), Bound::Included(y)) => (*x, y + 1),
+        (Bound::Included(x), Bound::Excluded(y)) => (*x, *y),
+        (Bound::Included(x), Bound::Unbounded) => (*x, count_elements),
+        (Bound::Unbounded, Bound::Unbounded) => (0, count_elements),
+        (Bound::Unbounded, Bound::Included(y)) => (0, y + 1),
+        (Bound::Unbounded, Bound::Excluded(y)) => (0, *y),
+        (Bound::Excluded(_), Bound::Unbounded)
+        | (Bound::Excluded(_), Bound::Included(_))
+        | (Bound::Excluded(_), Bound::Excluded(_)) => {
+            unreachable!("A start bound can't be excluded")
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use owo_colors::{colors::*, OwoColorize};
+
+    #[test]
+    fn cut_str() {
+        assert_eq!("som", cut("something", ..3));
+        assert_eq!("some", cut("something", ..=3));
+        assert_eq!("et", cut("something", 3..5));
+        assert_eq!("eth", cut("something", 3..=5));
+        assert_eq!("ething", cut("something", 3..));
+        assert_eq!("something", cut("something", ..));
+        assert_eq!("", cut("", ..));
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_on_index_higher_then_length() {
+        cut("qwe", ..5);
+    }
+
+    #[test]
+    fn cut_colored_str() {
+        let s = "something".fg::<Black>().bg::<Blue>().to_string();
+        assert_eq!("som".fg::<Black>().bg::<Blue>().to_string(), cut(&s, ..3));
+        assert_eq!("some".fg::<Black>().bg::<Blue>().to_string(), cut(&s, ..=3));
+        assert_eq!("et".fg::<Black>().bg::<Blue>().to_string(), cut(&s, 3..5));
+        assert_eq!("eth".fg::<Black>().bg::<Blue>().to_string(), cut(&s, 3..=5));
+        assert_eq!(
+            "ething".fg::<Black>().bg::<Blue>().to_string(),
+            cut(&s, 3..)
+        );
+    }
+
+    #[test]
+    fn cut_partially_colored_str() {
+        let s = format!("zxc_{}_qwe", "something".fg::<Black>().bg::<Blue>());
+        assert_eq!("zxc", cut(&s, ..3));
+        assert_eq!(
+            format!("zxc_{}", "s".fg::<Black>().bg::<Blue>()),
+            cut(&s, ..5)
+        );
+        assert_eq!(
+            "ometh".fg::<Black>().bg::<Blue>().to_string(),
+            cut(&s, 5..10)
+        );
+        assert_eq!(
+            format!("{}_qwe", "g".fg::<Black>().bg::<Blue>()),
+            cut(&s, 12..)
+        );
+    }
+
+    #[test]
+    fn cut_emojies() {
+        assert_eq!("😀😃😄", cut("😀😃😄😁😆😅😂🤣🥲😊", ..3));
+        assert_eq!("😅😂", cut("😀😃😄😁😆😅😂🤣🥲😊", 5..7));
+        assert_eq!("😊", cut("😀😃😄😁😆😅😂🤣🥲😊", 9..));
+        assert_eq!("🧑‍🏭", cut("🧑‍🏭🧑‍🏭🧑‍🏭", ..3));
+    }
+}
